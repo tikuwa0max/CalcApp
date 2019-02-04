050a202483e9d8770c934e9abe3e2bd81663d94e
tikuwa0max <itizawa@ruri.waseda.jp>
2019-02-04 21:18:08 +0900

    エラー

--- app/src/main/java/ichizawa/mikitaka/techacademy/calcapp/Second.kt
+++ app/src/main/java/ichizawa/mikitaka/techacademy/calcapp/Second.kt
@@ -2,11 +2,21 @@ package ichizawa.mikitaka.techacademy.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
 
-class Second : AppCompatActivity() {
+class Second : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val value1:Double = intent.getDoubleExtra("VALUE1", 0.0)
+        val value2:Double = intent.getDoubleExtra("VALUE2", 0.0)
+
+        textView.text = "${value1 + value2}"
+
     }
+
+
 }
