30fdd1d27acfd08f7a45d3488912d4c3f803e379
tikuwa0max <itizawa@ruri.waseda.jp>
2019-02-04 21:05:40 +0900

    Initial commit

---
+++ app/src/main/java/ichizawa/mikitaka/techacademy/calcapp/MainActivity.kt
@@ -0,0 +1,57 @@
+package ichizawa.mikitaka.techacademy.calcapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, Second::class.java)
+
+        var x:Float
+        var y:Float
+
+        x = editText.toString().toFloat()
+        y = editText2.toString().toFloat()
+        intent.putExtra("VALUE1", x)
+        intent.putExtra("VALUE2", y)
+        startActivity(intent)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/ichizawa/mikitaka/techacademy/calcapp/Second.kt
@@ -0,0 +1,12 @@
+package ichizawa.mikitaka.techacademy.calcapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Second : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
